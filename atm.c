24252e10a585acbc85c7c5884c391389efeadc8c
Aman-Kasa <a.kasa@alustudent.com>
2025-09-30 20:05:10 +0200

    structure of the main code

---
+++ atm.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+// Function declarations
+void login();
+void checkBalance();
+void deposit();
+void withdraw();
+void viewTransactions();
+void logout();
+
+int main() {
+    // Main menu goes here
+    return 0;
+}
